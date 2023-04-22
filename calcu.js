4f9eafb852ed3ffa87d0518ecd687b0da6a0b749
irakeshr <iamrakeshr0@gmail.com>
2023-04-22 14:32:45 +0530

    added js function

--- calcu.js
+++ calcu.js
@@ -1,13 +1,27 @@
-let one=document.querySelector("#one");
-let two = document.querySelector("#two");
-//   let subme=document.querySelector(".subme");
-let input=document.querySelector('input');
-   
-   
-one.onclick=()=>{
-  input.value=1;
-  console.log(input.value);
-} ;
-two.onclick=()=>{
-  input.value+=2;
-}
+  
+let input=document.querySelector("input");
+let clearbut=document.querySelector("#clear");
+let backspace=document.querySelector("#backspace");
+let equal=document.querySelector("#equal")
+
+function myfunction(clickednumber){
+  input.value+=clickednumber;
+  
+};
+
+clearbut.onclick=()=>{
+  input.value="";
+};
+
+
+backspace.onclick=()=>{
+  input.value=input.value.substring(0,input.value.length-1);
+};
+
+
+equal.onclick=()=>{
+  let values=input.value;
+  let result= eval(values);
+  console.log(result);
+  input.value=result;
+};
